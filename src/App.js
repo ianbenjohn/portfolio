cfdff9718db1471551c7ec642e4519b1ebd13528
ianbenjohn <58613328+ianbenjohn@users.noreply.github.com>
2020-07-29 13:59:01 -0700

    going to help my roommate take some trash to the dump

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import CssBaseline from '@material-ui/core/CssBaseline';
 import './App.css';
 import Home from './components/index'
 
 function App() {
   return (
     <>
+    <CssBaseline />
       <Home />
     </>
   );
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,15 +5,24 @@ import {ArrowBack, AssignmentInd, Home, Apps, ContactMail} from '@material-ui/ic
 
 const Nav = () => {
     return (
+        <>
+        <Box component='div'>
+            <Avatar src='' alt='ian-avatar' />
+
+
+
+        </Box>
         <Box component='nav'>
             <AppBar position='static' style={{background: 'purple'}}>
                 <Toolbar>
                     <IconButton>
                         <ArrowBack style={{color: 'coral'}}/>
                     </IconButton>
+                    <Typography varient='h5'>IanBenJohn</Typography>
                 </Toolbar>
             </AppBar>
         </Box>
+        </>
     )
 }
 
