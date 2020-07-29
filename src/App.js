c2184f0f7d36490a2b46fd07a84d6f1a3d86d268
ianbenjohn <58613328+ianbenjohn@users.noreply.github.com>
2020-07-29 13:36:04 -0700

    nav component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Home from './components/index'
 
 function App() {
   return (
     <>
-      <h2>Welcome</h2>
+      <Home />
     </>
   );
 }
--- src/components/index.js
+++ src/components/index.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
     return (
         <div>
-            
+            <h2>Home COmponent</h2>
         </div>
     )
 }
