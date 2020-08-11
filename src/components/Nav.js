ad4d37d64853b4ab38f51915cfcbbe0c86fde3a5
ianbenjohn <58613328+ianbenjohn@users.noreply.github.com>
2020-08-11 13:42:33 -0700

    Making pull down for navbard

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,15 +1,67 @@
 import React from 'react'
-import {AppBar, Toolbar, ListItem, IconButton, ListItemText, Avatar, Divider, List, Typography, Box } from '@material-ui/core'
+import { makeStyles } from '@material-ui/core/styles'
+import {AppBar, Toolbar, ListItem, IconButton, ListItemText, Avatar, Divider, List, Typography, Box, ListItemIcon } from '@material-ui/core'
 import {ArrowBack, AssignmentInd, Home, Apps, ContactMail} from '@material-ui/icons'
+import avatar from '../ianAvatar.png'
+
+//CSS Styles
+const useStyles = makeStyles(theme=>({
+    menuSliderContainer: {
+        width: 250,
+        background: "#511",
+        height: "30rem"
+    },
+    avatar: {
+        display: 'block',
+        margin: '0.5rem auto',
+        width: theme.spacing(13),
+        height: theme.spacing(13),
+
+    },
+    listItem: {
+        color: 'tan'
+    },
+}));
+
+const menuItems = [
+    {
+        listIcon: <Home />,
+        listText: 'Home'
+    },
+    {
+        listIcon: <AssignmentInd />,
+        listText: 'Resume'
+    },
+    {
+        listIcon: <Apps />,
+        listText: 'Portfolio'
+    },
+    {
+        listIcon: <ContactMail />,
+        listText: 'Contacts'
+    },
+]
 
 
 const Nav = () => {
+    const classes = useStyles()
     return (
         <>
-        <Box component='div'>
-            <Avatar src='' alt='ian-avatar' />
+        <Box className={classes.menuSliderContainer} component='div'>
+            <Avatar className={classes.avatar} src={avatar} alt='ian-avatar' />
+            <Divider />
+            <List>
+                {menuItems.map((lsItem, key) => (
+                    <ListItem button>
+                    <ListItemIcon className={classes.listItem}>
+                        {lsItem.listIcon}
 
-
+                    </ListItemIcon>
+                    <ListItemText primary={lsItem.listText}/>
+                    </ListItem>
+            
+                ))}
+            </List>
 
         </Box>
         <Box component='nav'>
@@ -18,7 +70,7 @@ const Nav = () => {
                     <IconButton>
                         <ArrowBack style={{color: 'coral'}}/>
                     </IconButton>
-                    <Typography varient='h5'>IanBenJohn</Typography>
+                    <Typography varient='h4'>IanBenJohn</Typography>
                 </Toolbar>
             </AppBar>
         </Box>
