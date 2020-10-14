e3c6027bb3916c6f02bdec7ed013891a28ef7b00
ianbenjohn <58613328+ianbenjohn@users.noreply.github.com>
2020-10-13 17:41:25 -0700

    Frames for 4 prjects. Just need to fill them in with valuable stuff

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -4,14 +4,27 @@ import { Box, Grid, Card, CardActionArea, CardActions, CardContent, CardMedia, B
 import { makeStyles } from '@material-ui/core/styles';
 import project1 from "../images/cangaroo-img.jpg"
 
+
+const useStyles = makeStyles({
+    mainContainer: {
+        height: "100%",
+    },
+    cardContainer: {
+        maxWidth: 345,
+        margin: "3rem",
+        margin: "5rem auto"
+    },
+});
+
 const Portfolio = () => {
+    const classes = useStyles()
     return (
-        <Box component="div">
+        <Box component="div" className={classes.mainContainer}>
             <Nav />
-            <Grid container justify='center' alignItems='center'>
+            <Grid container justify='center'>
                 {/* Project 1 */}
                 <Grid item xs={12} sm={8} md={6}>
-                    <Card>
+                    <Card className={classes.cardContainer}>
                         <CardActionArea>
                             <CardMedia
                             component='img'
@@ -27,7 +40,101 @@ const Portfolio = () => {
                                     ldjdnskafjndask jaksdjfh kasjdhf kasjdfh aksdjhf kasjdhfka jsdhf kajsdh fkj askdjghfsg
                                     ldjdnskafjndask jaksdjfh kasjdhf kasjdfh aksdjhf kasjdhfka jsdhf kajsdh fkj askdjghfsg
                                     ldjdnskafjndask jaksdjfh kasjdhf kasjdfh aksdjhf kasjdhfka jsdhf kajsdh fkj askdjghfsg
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Button size="small" color="primary">
+                                    Share
+                                </Button>
+                                <Button size="small" color="primary">
+                                    Live Demo
+                                </Button>
+                            </CardActions>
+                            
+                        </CardActionArea>
+                 </Card>
+                </Grid>
+                {/* Project 2 */}
+                <Grid item xs={12} sm={8} md={6}>
+                    <Card className={classes.cardContainer}>
+                        <CardActionArea>
+                            <CardMedia
+                            component='img'
+                            alt='Project 1'
+                            height='140'
+                            image={project1}
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5">
+                                    Project 2
+                                </Typography>
+                                <Typography  variant="body2" color="textSecondary" component="p">
                                     ldjdnskafjndask jaksdjfh kasjdhf kasjdfh aksdjhf kasjdhfka jsdhf kajsdh fkj askdjghfsg
+                                    ldjdnskafjndask jaksdjfh kasjdhf kasjdfh aksdjhf kasjdhfka jsdhf kajsdh fkj askdjghfsg
+                                    ldjdnskafjndask jaksdjfh kasjdhf kasjdfh aksdjhf kasjdhfka jsdhf kajsdh fkj askdjghfsg
+
+                                    
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Button size="small" color="primary">
+                                    Share
+                                </Button>
+                                <Button size="small" color="primary">
+                                    Live Demo
+                                </Button>
+                            </CardActions>
+                            
+                        </CardActionArea>
+                 </Card>
+                </Grid>
+                {/* Project 3 */}
+                <Grid item xs={12} sm={8} md={6}>
+                    <Card className={classes.cardContainer}>
+                        <CardActionArea>
+                            <CardMedia
+                            component='img'
+                            alt='Project 1'
+                            height='140'
+                            image={project1}
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5">
+                                    Project 3
+                                </Typography>
+                                <Typography  variant="body2" color="textSecondary" component="p">
+                                    ldjdnskafjndask jaksdjfh kasjdhf kasjdfh aksdjhf kasjdhfka jsdhf kajsdh fkj askdjghfsg
+                                    ldjdnskafjndask jaksdjfh kasjdhf kasjdfh aksdjhf kasjdhfka jsdhf kajsdh fkj askdjghfsg
+                                    ldjdnskafjndask jaksdjfh kasjdhf kasjdfh aksdjhf kasjdhfka jsdhf kajsdh fkj askdjghfsg
+                                </Typography>
+                            </CardContent>
+                            <CardActions>
+                                <Button size="small" color="primary">
+                                    Share
+                                </Button>
+                                <Button size="small" color="primary">
+                                    Live Demo
+                                </Button>
+                            </CardActions>
+                            
+                        </CardActionArea>
+                 </Card>
+                </Grid>
+                {/* Project 4 */}
+                <Grid item xs={12} sm={8} md={6}>
+                    <Card className={classes.cardContainer}>
+                        <CardActionArea>
+                            <CardMedia
+                            component='img'
+                            alt='Project 1'
+                            height='140'
+                            image={project1}
+                            />
+                            <CardContent>
+                                <Typography gutterBottom variant="h5">
+                                    Project 4
+                                </Typography>
+                                <Typography  variant="body2" color="textSecondary" component="p">
                                     ldjdnskafjndask jaksdjfh kasjdhf kasjdfh aksdjhf kasjdhfka jsdhf kajsdh fkj askdjghfsg
                                     ldjdnskafjndask jaksdjfh kasjdhf kasjdfh aksdjhf kasjdhfka jsdhf kajsdh fkj askdjghfsg
                                     ldjdnskafjndask jaksdjfh kasjdhf kasjdfh aksdjhf kasjdhfka jsdhf kajsdh fkj askdjghfsg
